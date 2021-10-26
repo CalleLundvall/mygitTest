274fb746c199aa297c28d84c3aba25487657d461
CalleLundvall <calle.lundvall@gmail.com>
2021-10-26 15:29:29 +0200

    projekt

---
+++ src/CharCount.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CharCount {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        int stringLength = input.length();
+        System.out.println("Bokstäver & tecken: " + stringLength);
+
+
+
+String i =null;
+        int line = 1;
+       i = scan.nextLine();
+        if (i!= null) {
+            line++;
+        }
+
+        System.out.println("rader: " +line);
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            String Str = sc.nextLine();
+            System.out.println(Str);
+
+            if(Str.equals("stop"))
+                break;
+
+        }
+
+
+
+
+
+
+
+    }
+}
